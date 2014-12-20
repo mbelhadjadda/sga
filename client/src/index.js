c6b4d07f44d9f4f71b700629951e936edd7fc150
mbelhadjadda <belhadj-adda.m@sfeir.com>
2014-12-20 11:08:26 +0100

    Ajout d'une methode pour gerer les evts

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,10 @@
 angular.module("sga", ["page1", "page2", "pageNt", "home"]).run(['$rootScope', '$location', '$window',
     function ($rootScope, $location, $window) {
 
+        $rootScope.eventTrackButtonSga = function (label) {
+            $window.ga('send', 'event', $location.path(), 'Click sur un button', label);
+        };
+
         $rootScope.noPageTracking = ["/pageNt"];
         $rootScope.$on('$routeChangeSuccess',
             function (event) {
